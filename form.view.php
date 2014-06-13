ac755630ffd79a3424e764b2aa98d6bf4052aa51
chenyinl <linchenyin@gmail.com>
2014-06-13 11:24:55 -0700

    Create form.view.php

---
+++ form.view.php
@@ -0,0 +1,7 @@
+<html>
+    <h2>Enter JSON here</h2>
+    <form action="#" method="post">
+    	<textarea rows = "30" cols="100" name = "jsonString"><?php if(isset($jsonString)) echo $jsonString; ?></textarea>
+    	<input type="submit" value = "convert">
+    </form>
+</html>
